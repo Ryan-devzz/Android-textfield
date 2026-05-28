69716cc13157d1f6b21f2e1de4241f643df241ed
Ryan <ryanfonsecaguedes@gmail.com>
2026-05-28 20:02:35 -0300

    Add files via upload

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            // A Column já garante o alinhamento à direita (End) e no meio (Center)
+            Column(
+                modifier = Modifier
+                    .background(color = Color.Yellow)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.End
+            ) {
+                // Estado compartilhado (iniciando vazio para ficar melhor na tela)
+                var vlrDoTextField by remember { mutableStateOf("") }
+
+                // Nomes importantes na história do computador (incluindo mulheres)
+                Text("Ada Lovelace")
+                Text("Alan Turing")
+                Text("Grace Hopper")
+                Text("Charles Babbage")
+                Text("Margaret Hamilton")
+
+                // Campos de texto replicados
+                TextField(vlrDoTextField, {
+                    vlrDoTextField = it
+                })
+
+                OutlinedTextField(vlrDoTextField, {
+                    Log.i("###", it) // Mantive o seu Log!
+                    vlrDoTextField = it
+                })
+            }
+        }
+    }
+}
